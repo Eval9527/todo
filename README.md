49a4f611fa5f73e7145888cc5111100713b0cd89
pooc <pooc95@163.com>
2021-06-27 23:38:26 +0800

    up: 更新 README.md 及相关注释

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
-# 面向对象、类的继承、横向切割程序
+# 使用 TypeScript + Vite 写的原生 TODO
+
+## 面向对象、类的继承、横向切割程序
 1. 程序进行分类  
     外层：浏览器的事件 -> 调用方法 -> 事件处理函数的绑定  
     操作数据：addTodo、removeTodo、toggleComplete  
     操作DOM：addItem、removeItem、changeCompleted  
     管理模版：todoView -> 接受参数
+
+
+## 使用
+```shell
+npm i
+npm run dev
+```
+
+## 说明
+项目结构
+```markdown
+todo
+ ├── index.html
+ └── src
+     ├── app.ts
+     └── js
+         ├── TodoDom.ts  // todo dom 操作
+         ├── utils.ts  // 工具类
+         ├── TodoEvent.ts  // todo 事件
+         ├── TodoTemplate.ts  // 渲染 todo-item 的 DOM 模板
+         └── typing.ts  // 接口
+```
--- src/js/utils.ts
+++ src/js/utils.ts
@@ -1,3 +1,8 @@
+/**
+ * 寻找当前父节点
+ * @param target
+ * @param className
+ */
 function findParentNode(target: HTMLElement, className: string): HTMLElement {
     while (target = target.parentNode as HTMLElement) {
         if (target.className === className) {
@@ -6,6 +11,12 @@ function findParentNode(target: HTMLElement, className: string): HTMLElement {
     }
 }
 
+/**
+ * 创建节点
+ * @param tagName
+ * @param className
+ * @param todoItem
+ */
 function createItem(tagName: string, className: string, todoItem: string): HTMLElement {
     const dItem: HTMLElement = document.createElement(tagName)
     dItem.className = className
